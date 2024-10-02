6165a728bf5ec01c96094f2e91852d9d0adbd735
Zena-art <zena.nazim@gmail.com>
2024-10-02 15:05:44 -0500

    win and lost messages are displayed

--- script.js
+++ script.js
@@ -129,14 +129,39 @@ class UI {
     this.game = game;
     this.fontSize = 25;
     this.fontFamily = "Helvetica";
-    this.color = 'yellow';
+    this.color = 'white';
   }
   draw(context){
-    // ammo
+    context.save();
+    context.shadowOffsetX = 2;
+    context.shadowOffsetY = 2;
+    context.shadowColor = 'black';
     context.fillStyle = this.color;
+    context.font = this.fontSize + 'px' + this.fontFamily;
+    //score
+    context.fillText('Score: ' + this.game.score, 20, 40);
+    //ammo
     for(let i = 0; i < this.game.ammo; i++){
       context.fillRect(20 + 5 * i, 50, 3, 20);
     }
+    // game over messages
+    if(this.game.gameOver){
+      context.textAlign = 'center';
+      let message1;
+      let message2;
+      if(this.game.score > this.game.winningScore){
+        message1 = 'You win!';
+        message2 = 'Well done!';
+      }else {
+        message1 = 'You lose!';
+        message2 = 'Try again next time';
+      }
+      context.font = '50px ' + this.fontFamily;
+      context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5);
+      context.font = '25px ' + this.fontFamily;
+      context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5);
+    }
+    context.restore();
   }
 }
 class Game {
@@ -178,6 +203,7 @@ class Game {
           if(enemy.lives <= 0){
             enemy.markedForDeletion = true;
             this.score += enemy.score;
+            if( this.score > this.winningScore) this.gameOver = true;
           }
         }
       })
