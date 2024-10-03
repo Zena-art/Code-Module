faf01b3d93fe97f006772cd2f8ebfdfa269c1209
Zena-art <zena.nazim@gmail.com>
2024-10-02 21:27:45 -0500

    player animation

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ class InputHandler {
           this.game.keys.push(e.key);
         }else if( e.key ===  ' '){
           this.game.player.shootTop();
+        }else if (e.key === 'd'){
+          this.game.debug = !this.game.debug;
         }
        
       });
@@ -56,9 +58,13 @@ class Player {
       this.height = 190;
       this.x = 20;
       this.y = 100;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrame = 37;
       this.speedY = 0;
       this.maxSpeed = 2;
       this.projectiles = [];
+      this.image = document.getElementById('player');
     }
     update(){
       if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
@@ -70,10 +76,15 @@ class Player {
         projectile.update();
       });
       this.projectiles = this.projectiles.filter(projectle => !projectle.markedForDeletion);
+      if(this.frameX < this.maxFrame){
+        this.frameX ++;
+      }else{
+        this.frameX = 0;
+      }
     }
     draw(context){
-      context.fillStyle = 'black';
-      context.fillRect(this.x, this.y, this.width, this.height);
+      if(this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+      context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
       this.projectiles.forEach(projectile => {
         projectile.draw(context);
       })
@@ -129,13 +140,34 @@ class Layer {
   }
   update(){
     if(this.x <= -this.width) this.x = 0;
-    else this.x -= this.game.speed * this.speedModifier;
+    this.x -= this.game.speed * this.speedModifier;
+  }
+  draw(content){
+    content.drawImage(this.image, this.x, this.y);
+    content.drawImage(this.image, this.x + this.width, this.y);
   }
 
 
 }
 class Background {
-
+  constructor(game){
+    this.game = game;
+    this.image1 = document.getElementById('layer1');
+    this.image2 = document.getElementById('layer2');
+    this.image3 = document.getElementById('layer3');
+    this.image4 = document.getElementById('layer4');
+    this.layer1 = new Layer(this.game, this.image1, 0.2);
+    this.layer2 = new Layer(this.game, this.image2, 0.4);
+    this.layer3 = new Layer(this.game, this.image3, 1);
+    this.layer4 = new Layer(this.game, this.image4, 1.3);
+    this.layers = [this.layer1, this.layer2, this.layer3];
+  }
+  update(){
+    this.layers.forEach(layer => layer.update());
+  }
+  draw(context){
+    this.layers.forEach(layer => layer.draw(context));
+  }
 }
 class UI {
   constructor(game){
@@ -184,6 +216,7 @@ class Game {
   constructor(width, height){
     this.width = width;
     this.height = height;
+    this.background = new Background(this);
     this.player = new Player(this);
     this.input = new InputHandler(this);
     this.ui = new UI(this);
@@ -201,10 +234,13 @@ class Game {
     this.gameTime = 0;
     this.timeLimit = 5000;
     this.speed = 1;
+    this.debug = true;
   }
   update(deltaTime){
     if(!this.gameOver) this.gameTime += deltaTime;
     if(this.gameTime > this.timeLimit) this.gameOver = true;
+    this.background.update();
+    this.background.layer4.update();
     this.player.update();
     if(this.ammoTimer > this.ammoInterval) {
       if (this.ammo < this.maxAmmo) this.ammo++;
@@ -239,11 +275,13 @@ class Game {
     }
   }
   draw(context){
+    this.background.draw(context);
     this.player.draw(context);
     this.ui.draw(context);
     this.enemies.forEach(enemy => {
       enemy.draw(context);
     });
+    this.background.layer4.draw(context);
   }
   addEnemy(){
     this.enemies.push(new Angler1(this));
